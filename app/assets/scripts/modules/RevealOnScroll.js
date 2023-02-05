e77b7f6aeabd9e0fce4bcd347c687135f1d6db1b
Andrey <104921155+AndreyPerunov@users.noreply.github.com>
2023-02-05 17:28:38 +0200

    Completed making sticky header

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -15,7 +15,6 @@ class RevealOnScroll {
     window.addEventListener(
       "resize",
       debounce(() => {
-        console.log("Resize just ran")
         this.browserHeight = window.innerHeight
       }, 333)
     )
